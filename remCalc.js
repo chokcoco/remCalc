ca036445fba60d233bb6b67df55fda89a6b36670
coco <308695699@qq.com>
2016-02-19 15:43:05 +0800

    update

--- remCalc.js
+++ remCalc.js
@@ -1,18 +1,16 @@
-// 1rem -> 10px
-(function (doc, win) {
-    var docEl = doc.documentElement,
-        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-        recalc = function () {
-            var clientWidth = docEl.clientWidth;
-            if (!clientWidth) return;
-            if(clientWidth>=640){
-                docEl.style.fontSize = '10px';
-            }else{
-                docEl.style.fontSize = 10 * (clientWidth / 320) + 'px';
-            }
-        };
+// 以 320 的设备宽度为基准，320 下 10px 为 1rem
+(function(doc, win) {
+  var docEl = doc.documentElement,
+    resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+    recalc = function() {
+      var clientWidth = docEl.clientWidth;
+      if (!clientWidth) {
+        return;
+      }
+      docEl.style.fontSize = 10 * (clientWidth / 320) + 'px';
+    };
 
-    if (!doc.addEventListener) return;
-    win.addEventListener(resizeEvt, recalc, false);
-    doc.addEventListener('DOMContentLoaded', recalc, false);
+  if (!doc.addEventListener) return;
+  win.addEventListener(resizeEvt, recalc, false);
+  doc.addEventListener('DOMContentLoaded', recalc, false);
 })(document, window);
