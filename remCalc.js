e843c24988a8cf6dcb157ad039e0e84fa5b500d5
coco <308695699@qq.com>
2016-02-19 15:29:47 +0800

    update

---
+++ remCalc.js
@@ -0,0 +1,18 @@
+// 1rem -> 10px
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            if(clientWidth>=640){
+                docEl.style.fontSize = '10px';
+            }else{
+                docEl.style.fontSize = 10 * (clientWidth / 320) + 'px';
+            }
+        };
+
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
